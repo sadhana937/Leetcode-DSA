45ac61ade3f5dd1c33bff469ab44ce5066eb363b
Sadhana K S <117561777+sadhana937@users.noreply.github.com>
2023-09-15 23:12:54 +0530

    Create GCDofStringsBruteForce.py

---
+++ Strings/GCDofStringsBruteForce.py
@@ -0,0 +1,16 @@
+# Greatest Common Divisor of Strings - Brute Force
+# Time complexity: O(n)
+# Space complexity: O(1)
+
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        str1_len, str2_len = len(str1), len(str2)
+        
+        for i in range(min(str1_len,str2_len),0,-1):
+            if not (str1_len % i or str2_len % i): # if the strings are divisible the base string length
+                x = str1[:i] # x -> base string
+                len1 = str1_len // i
+                len2 = str2_len // i
+                if str1 == len1 * x and str2 == len2 * x:
+                    return x
+        return ''            
