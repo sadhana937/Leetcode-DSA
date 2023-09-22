c79a012b14924f6ad714313730a1c3d2b58a3ef3
Sadhana K S <117561777+sadhana937@users.noreply.github.com>
2023-09-22 23:44:58 +0530

    Update AddDigits.py

--- Math/AddDigits.py
+++ Math/AddDigits.py
@@ -6,8 +6,11 @@ class Solution1:
     def addDigits(self, num: int) -> int:
         return 0 if num == 0 else (9 if num % 9 == 0 else num % 9) 
 
+
+
 # Time complexity: O(n)
 # Space complexity: O(1)
+
 class Solution2:
     def addDigits(self, num: int) -> int:
         digital_root = 0
