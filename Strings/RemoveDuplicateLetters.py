e44616a8141da83aeabf1bf0190f3e529f0e6a0d
Sadhana K S <117561777+sadhana937@users.noreply.github.com>
2023-09-26 22:49:49 +0530

    Create RemoveDuplicateLetters.py

---
+++ Strings/RemoveDuplicateLetters.py
@@ -0,0 +1,17 @@
+# Return the smallest lexicographical order of the string after removing the duplicates
+# Time complexity: O(n)
+# Space complexity: O(n)
+
+class Solution:
+    def removeDuplicateLetters(self, s: str) -> str:
+        stack = []
+        seen = set()
+        last_occurrence = {char : i for i, char in enumerate(s)}
+
+        for i, char in enumerate(s):
+            if char not in seen:
+                while stack and char < stack[-1] and i < last_occurrence[stack[-1]]:
+                    seen.discard(stack.pop())
+                stack.append(char)
+                seen.add(char)
+        return "".join(stack)
