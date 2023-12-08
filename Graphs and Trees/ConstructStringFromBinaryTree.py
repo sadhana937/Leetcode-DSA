14e7dbc2cf15bf0f9b0368af2f04b51a9a4bb6d6
Sadhana K S <117561777+sadhana937@users.noreply.github.com>
2023-12-08 22:35:17 +0530

    Create ConstructStringFromBinaryTree.py

---
+++ Graphs and Trees/ConstructStringFromBinaryTree.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def tree2str(self, root: Optional[TreeNode]) -> str:
+        if root is None:
+            return ''
+        ans = str(root.val)
+        if root.left:
+            ans += '({})'.format(self.tree2str(root.left))
+        if root.left is None and root.right:
+            ans += '()'
+        if root.right:
+            ans += '({})'.format(self.tree2str(root.right))
+        return ans
