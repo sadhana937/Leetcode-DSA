ddaa4bc6158915880cc85fcc15f46fa60385effd
Sadhana K S <117561777+sadhana937@users.noreply.github.com>
2024-03-08 21:19:35 +0530

    Update RemoveOneElementToMakeStrictlyIncreasing.py

--- Arrays/RemoveOneElementToMakeStrictlyIncreasing.py
+++ Arrays/RemoveOneElementToMakeStrictlyIncreasing.py
@@ -1,4 +1,4 @@
-class Solution:
+class Solution1:
     def canBeIncreasing(self, nums: List[int]) -> bool:
         deleted = False
         i = 1
@@ -15,3 +15,16 @@ class Solution:
                 prev = nums[i]
                 i += 1
         return True
+
+class Solution2:
+    def canBeIncreasing(self, nums: List[int]) -> bool:
+        removed = False
+        for i in range(1, len(nums)):
+            if nums[i] <= nums[i - 1]:
+                if removed:
+                    return False
+                if i > 1 and nums[i] <= nums[i - 2]:
+                    nums[i] = nums[i - 1]
+                removed = True
+        return True
+
