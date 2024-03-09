b1aa006370330014d2c135921ce2b330d534a2f8
Sadhana K S <117561777+sadhana937@users.noreply.github.com>
2024-03-09 15:33:51 +0530

    Update MinimumCommonValue.py

--- Arrays/MinimumCommonValue.py
+++ Arrays/MinimumCommonValue.py
@@ -1,8 +1,19 @@
-class Solution:
+class Solution1:
     def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
         nums2 = set(nums2)
         for i in nums1:
             if i in nums2:
                 return i
         return -1
+
+
+class Solution2:
+    def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
+        nums1 = set(nums1)
+        nums2 = set(nums2)
+        common = nums1.intersection(nums2)
+        if common:
+            return min(common)
+        else:
+            return -1
 
