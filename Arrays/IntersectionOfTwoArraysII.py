a83681f24b32df1027664a395a7249f57d4bdfce
Sadhana K S <117561777+sadhana937@users.noreply.github.com>
2024-07-02 09:40:43 +0530

    Update IntersectionOfTwoArraysII.py

--- Arrays/IntersectionOfTwoArraysII.py
+++ Arrays/IntersectionOfTwoArraysII.py
@@ -1,4 +1,14 @@
-class Solution:
+class Solution1:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        hashmap = Counter(nums1)
+        output = []
+        for i in nums2:
+            if hashmap[i] > 0:
+                output.append(i)
+                hashmap[i] -= 1
+        return output
+        
+class Solution2:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
         set1 = set(nums1)
         set2 = set(nums2)
