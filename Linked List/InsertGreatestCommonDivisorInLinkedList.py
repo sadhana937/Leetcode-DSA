16feed434ed3bfbd9acdeaf6ef5daf67fa3be240
Sadhana K S <117561777+sadhana937@users.noreply.github.com>
2024-09-10 20:25:57 +0530

    Create InsertGreatestCommonDivisorInLinkedList.py

---
+++ Linked List/InsertGreatestCommonDivisorInLinkedList.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def insertGreatestCommonDivisors(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        cur = head
+        
+        while cur and cur.next:
+            temp = ListNode(gcd(cur.val, cur.next.val), cur.next)
+            cur.next = temp
+            cur = temp.next
+        
+        return head
+
+        
